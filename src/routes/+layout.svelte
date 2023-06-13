c113fa45f316bb40bccdbcf1f940a0b8f4f0c3e1
Альфия Бадгутова <badgutova.alfiya@gmail.com>
2023-06-13 21:38:46 +1000

    add todo app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<nav>
+    <div class="nav-container">
+      <a href="/" class="nav-logo" title="Back to Homepage">Бадгутова Альфия</a>
+      <div class="nav-links">
+        {#each nav as link}
+<a href={link.href} class="link">{link.title}</a>
+{/each}
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+  </div>
+  <svelte:head>
+    <title>Бадгутова Альфия</title>
+    </svelte:head>
+<script>
+        const nav = [
+        {
+          title: 'Обо мне',
+          href: '/about'
+        },
+        {
+          title: 'Блог',
+          href: '/blog'
+        },
+        {
+          title: 'Портфолио',
+          href: '/projects'
+        },
+        {
+          title: 'Контакты',
+          href: '/contacts'
+        },
+      ]
+</script>
+    
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,19 @@
+<div class="container">
+	<h1>Контакты</h1>
+<br>
+	<ul>
+		<li>Моя электронная почта (@gmail.com): badgutova.alfiya<br></li>
+		<li>Мой ник в Телеграме(t.me/): Alter_ego_Pechorin<br></li>
+	</ul>
+<br>
+Я открыта для общения и со мной всегда можно связаться любым из этих двух способов)
+</div>
+
+<style>
+.container {
+		margin: 50px auto;
+		max-width: 800px;
+		padding: 0 20px;
+	}
+
+</style>
